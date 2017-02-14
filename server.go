37468e7d4d9f9b6733923e6f53c4c622e0a28f59
Addison Luh <hueich@gmail.com>
2017-02-13 21:38:00 -0800

    Use new object oriented service model

--- server.go
+++ server.go
@@ -1,16 +1,33 @@
 package main
 
 import (
+	"context"
+	"flag"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	bs "github.com/hueich/blokus-service"
 )
 
+var (
+	port = flag.Int("port", 9090, "Port to start the server on")
+)
+
 func main() {
-	r := bs.NewRouter("/blokus/")
-	http.Handle("/", r)
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	log.Println("DATASTORE_PROJECT_ID:", os.Getenv("DATASTORE_PROJECT_ID"))
+	log.Println("GOOGLE_APPLICATION_CREDENTIALS:", os.Getenv("GOOGLE_APPLICATION_CREDENTIALS"))
+
+	s := bs.New("/blokus/")
+	if err := s.InitClient(context.Background(), "", ""); err != nil {
+		log.Fatalf("Could not create client: %v\n", err)
+	}
+	defer s.Close()
+
+	http.Handle("/", s.Router())
+	log.Println("Starting to listen on port", *port)
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", *port), nil); err != nil {
 		log.Fatalf("Could not start server: %v\n", err)
 	}
 }
