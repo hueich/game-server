27b9e2b9005598194ef6063ca05d3fee7bf10c76
Addison Luh <hueich@gmail.com>
2017-02-18 22:12:43 -0800

    Update server to use new service api and package name update

--- server.go
+++ server.go
@@ -8,7 +8,8 @@ import (
 	"net/http"
 	"os"
 
-	bs "github.com/hueich/blokus-service"
+	"github.com/gorilla/mux"
+	bapi "github.com/hueich/blokus-web-api"
 )
 
 var (
@@ -19,13 +20,18 @@ func main() {
 	log.Println("DATASTORE_PROJECT_ID:", os.Getenv("DATASTORE_PROJECT_ID"))
 	log.Println("GOOGLE_APPLICATION_CREDENTIALS:", os.Getenv("GOOGLE_APPLICATION_CREDENTIALS"))
 
-	s := bs.New("/blokus/")
-	if err := s.InitClient(context.Background(), "", ""); err != nil {
-		log.Fatalf("Could not create client: %v\n", err)
+	r := mux.NewRouter()
+	br := r.PathPrefix("/blokus").Subrouter()
+	s, err := bapi.NewService(br.PathPrefix("/api").Subrouter())
+	if err != nil {
+		log.Fatalf("Could not create service: %v\n", err)
 	}
 	defer s.Close()
+	if err := s.InitClient(context.Background(), "", ""); err != nil {
+		log.Fatalf("Could not initialize client: %v\n", err)
+	}
 
-	http.Handle("/", s.Router())
+	http.Handle("/", r)
 	log.Println("Starting to listen on port", *port)
 	if err := http.ListenAndServe(fmt.Sprintf(":%d", *port), nil); err != nil {
 		log.Fatalf("Could not start server: %v\n", err)
