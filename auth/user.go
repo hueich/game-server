bbf625343eea96239a0b595f91ae2598b46dac1a
Addison Luh <hueich@gmail.com>
2017-02-28 23:00:59 -0800

    Added datatastore client creation to user manager

--- auth/user.go
+++ auth/user.go
@@ -1,13 +1,20 @@
 package auth
 
 import (
+	"context"
+
 	"cloud.google.com/go/datastore"
+	"github.com/hueich/game-server/db"
 )
 
 type UserManager struct {
 	client *datastore.Client
 }
 
-func NewUserManager() (*UserManager, error) {
-	return &UserManager{}
+func NewUserManager(ctx context.Context, projectID, credsFile string) (*UserManager, error) {
+	c, err := db.NewDatastoreClient(ctx, projectID, credsFile)
+	if err != nil {
+		return nil, err
+	}
+	return &UserManager{client: c}, nil
 }
