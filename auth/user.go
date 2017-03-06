ac0097410769c482a44009db16cffacae3b242ae
Addison Luh <hueich@gmail.com>
2017-03-05 18:27:43 -0800

    Implement new user flow

--- auth/user.go
+++ auth/user.go
@@ -2,10 +2,17 @@ package auth
 
 import (
 	"context"
+	"fmt"
+	"net/mail"
 	"time"
 
 	"cloud.google.com/go/datastore"
 	"github.com/hueich/game-server/db"
+	"golang.org/x/crypto/bcrypt"
+)
+
+const (
+	MinPasswordLength = 6
 )
 
 type userInfo struct {
@@ -28,12 +35,53 @@ func NewUserManager(ctx context.Context, projectID, credsFile string) (*UserMana
 	return &UserManager{client: c}, nil
 }
 
-func (m *UserManager) AddUser(username string, password []byte, email string) error {
-	// TODO: Check for existing user in db
+func (m *UserManager) AddUser(ctx context.Context, username string, password []byte, email string) error {
+	if username == "" {
+		return fmt.Errorf("username cannot be empty")
+	}
+	if len(password) < MinPasswordLength {
+		return fmt.Errorf("password is too short")
+	}
+	if email != "" {
+		addr, err := mail.ParseAddress(email)
+		if err != nil {
+			return fmt.Errorf("email is invalid")
+		}
+		email = addr.Address
+	}
 
-	// TODO: Hash password with bcrypt
+	_, err := m.client.RunInTransaction(ctx, func(tx *datastore.Transaction) error {
+		// TODO: Check for existing user in db
+		q := datastore.NewQuery("User").Transaction(tx).Limit(1).Filter("Username =", username)
+		n, err := m.client.Count(ctx, q)
+		if err != nil {
+			return err
+		}
+		if n > 0 {
+			return fmt.Errorf("user already exists")
+		}
 
-	// TODO: Add user to db
+		hash, err := bcrypt.GenerateFromPassword(password, bcrypt.DefaultCost)
+		if err != nil {
+			return fmt.Errorf("failed to generate hash: %v", err)
+		}
 
+		u := &userInfo{
+			Username:     username,
+			PasswordHash: hash,
+			Email:        email,
+			TimeCreated:  time.Now(),
+			TimeLastSeen: time.Now(),
+		}
+
+		key := datastore.IncompleteKey("User", nil)
+		if _, err := tx.Put(key, u); err != nil {
+			return err
+		}
+		return nil
+	})
+	if err != nil {
+		return fmt.Errorf("failed to add new user: %v", err)
+	}
 	return nil
 }
