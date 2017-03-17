cfc64b462f0bd5849d66d60f137a5e6d04773f74
Addison Luh <hueich@gmail.com>
2017-03-16 19:44:19 -0700

    Add new session manager options for session name

--- auth/session.go
+++ auth/session.go
@@ -6,16 +6,30 @@ import (
 	"github.com/gorilla/sessions"
 )
 
+const (
+	DefaultSessionName = "gs"
+)
+
 type SessionManager struct {
+	Name string
+
 	store sessions.Store
 }
 
-func NewSessionManager(store sessions.Store) (*SessionManager, error) {
+type SessionManagerOptions struct {
+	SessionName string
+}
+
+func NewSessionManager(store sessions.Store, opt SessionManagerOptions) (*SessionManager, error) {
 	if store == nil {
 		return nil, errors.New("session store cannot be nil")
 	}
 	m := &SessionManager{
+		Name:  DefaultSessionName,
 		store: store,
+	}
+	if opt.SessionName != "" {
+		m.Name = opt.SessionName
 	}
 	return m, nil
 }
