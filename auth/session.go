981058dc12a3d34aaf45b8eed8002f012368e166
Addison Luh <hueich@gmail.com>
2017-03-18 22:30:25 -0700

    Added GetUsername() function to SessionManager

--- auth/session.go
+++ auth/session.go
@@ -2,12 +2,17 @@ package auth
 
 import (
 	"errors"
+	"net/http"
 
 	"github.com/gorilla/sessions"
 )
 
 const (
-	DefaultSessionName = "gs"
+	defaultSessionName = "ss"
+)
+
+var (
+	ErrNotLoggedIn = errors.New("session: user not logged in")
 )
 
 type SessionManager struct {
@@ -20,16 +25,33 @@ type SessionManagerOptions struct {
 	SessionName string
 }
 
-func NewSessionManager(store sessions.Store, opt SessionManagerOptions) (*SessionManager, error) {
+func NewSessionManager(store sessions.Store, opt *SessionManagerOptions) (*SessionManager, error) {
 	if store == nil {
-		return nil, errors.New("session store cannot be nil")
+		return nil, errors.New("session: store cannot be nil")
 	}
 	m := &SessionManager{
-		Name:  DefaultSessionName,
+		Name:  defaultSessionName,
 		store: store,
 	}
-	if opt.SessionName != "" {
-		m.Name = opt.SessionName
+	if opt != nil {
+		if opt.SessionName != "" {
+			m.Name = opt.SessionName
+		}
 	}
 	return m, nil
+}
+
+func (m *SessionManager) GetUsername(r *http.Request) (string, error) {
+	s, err := m.store.Get(r, m.Name)
+	if err != nil {
+		return "", err
+	}
+	if s.IsNew {
+		return "", ErrNotLoggedIn
+	}
+	username, ok := s.Values["username"].(string)
+	if !ok || username == "" {
+		return "", errors.New("session: username is empty")
+	}
+	return username, nil
 }
